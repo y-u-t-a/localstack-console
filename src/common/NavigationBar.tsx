4eec45450ee46dc030e7ab4e859066b3dd3b0571
Yuta Ueno <y.ueno0803@gmail.com>
2020-05-15 06:16:11 +0900

    ドロップダウンの内容をそれっぽく変更

--- src/common/NavigationBar.tsx
+++ src/common/NavigationBar.tsx
@@ -9,14 +9,10 @@ export default class NavigationBar extends React.Component {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            <NavDropdown title="Services" id="nav-dropdown">
+              <NavDropdown.Item href="/ec2">EC2</NavDropdown.Item>
+              <NavDropdown.Item href="/s3">S3</NavDropdown.Item>
+              <NavDropdown.Item href="/vpc">VPC</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
