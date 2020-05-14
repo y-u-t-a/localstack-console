5126a2049a2e7564309a4f5c6357f9e3ef80493d
Yuta Ueno <y.ueno0803@gmail.com>
2020-05-15 06:26:33 +0900

    消しても問題なさそうな記述を削除

--- src/common/NavigationBar.tsx
+++ src/common/NavigationBar.tsx
@@ -6,16 +6,13 @@ export default class NavigationBar extends React.Component {
     return (
       <Navbar bg="light" expand="lg">
         <Navbar.Brand href="/">AWS Mock</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <NavDropdown title="Services" id="nav-dropdown">
-              <NavDropdown.Item href="/ec2">EC2</NavDropdown.Item>
-              <NavDropdown.Item href="/s3">S3</NavDropdown.Item>
-              <NavDropdown.Item href="/vpc">VPC</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
+        <Nav>
+          <NavDropdown title="Services" id="nav-dropdown">
+            <NavDropdown.Item href="/ec2">EC2</NavDropdown.Item>
+            <NavDropdown.Item href="/s3">S3</NavDropdown.Item>
+            <NavDropdown.Item href="/vpc">VPC</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
       </Navbar>
     )
   }
