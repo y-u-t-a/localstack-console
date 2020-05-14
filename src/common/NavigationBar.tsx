0f0ae33292feaecba71a6db318c7ea8585889065
Yuta Ueno <y.ueno0803@gmail.com>
2020-05-15 06:01:54 +0900

    navbar の左上の文字とリンクを修正

--- src/common/NavigationBar.tsx
+++ src/common/NavigationBar.tsx
@@ -5,7 +5,7 @@ export default class NavigationBar extends React.Component {
   render() {
     return (
       <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="/">AWS Mock</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
