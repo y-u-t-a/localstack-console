aebc14f543d0463679c08a47e577dd6108a3b4f3
Yuta Ueno <y.ueno0803@gmail.com>
2020-05-16 03:24:25 +0900

    Vpc コンポーネントを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import Top from './top/Top'
+import Vpc from './vpc/Vpc'
 
 export default class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <Route exact path='/' component={Top} />
+        <Route exact path='/vpc' component={Vpc} />
       </BrowserRouter>
     )
   }
---
+++ src/vpc/Vpc.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import AWS from 'aws-sdk'
+
+export default class Vpc extends React.Component {
+  render() {
+    getVpc()
+    return (
+      <div>
+        <h3>VPC一覧</h3>
+      </div>
+    )
+  }
+}
+
+function getVpc() {
+  const credential = new AWS.Credentials({ accessKeyId: 'dummy', secretAccessKey: 'dummy', sessionToken: 'dummy' });
+  const ec2 = new AWS.EC2({endpoint: "http://localhost:4566", region: "us-east-1", credentials: credential})
+  ec2.describeVpcs(function(err, data) {
+    if (err) { 
+      console.log(err, err.stack)
+    } else {
+      console.log(data.Vpcs)
+    }
+  })
+}
