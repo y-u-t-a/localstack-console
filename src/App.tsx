28f784da24c5eb1999649cf1e617c878d05056d6
Yuta Ueno <y.ueno0803@gmail.com>
2020-05-16 03:24:10 +0900

    お試しで書いた aws-sdk 呼び出し処理を削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import Top from './top/Top'
-import AWS from 'aws-sdk'
 
 export default class App extends React.Component {
   render() {
-    const credential = new AWS.Credentials({
-      accessKeyId: 'dummy', secretAccessKey: 'dummy', sessionToken: 'dummy'
-    });
-    const ec2 = new AWS.EC2({endpoint: "http://localhost:4566", region: "us-east-1", credentials: credential})
-    ec2.describeVpcs(function(err, data) {
-      if (err) { 
-        console.log(err, err.stack)
-      } else {
-        console.log(data.Vpcs)
-      }
-    })
     return (
       <BrowserRouter>
         <Route exact path='/' component={Top} />
