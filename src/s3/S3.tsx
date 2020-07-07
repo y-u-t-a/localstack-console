f186e6fcff9b8d2df1664a838c735bbf09ff8b3d
Yuta Ueno <30320735+y-u-t-a@users.noreply.github.com>
2020-07-08 03:27:17 +0900

    S3 バケット一覧を取得する処理を追加

--- src/s3/S3.tsx
+++ src/s3/S3.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Const from '../common/Const'
 import S3BucketList from './S3BucketList'
+import AwsClients from '../common/AwsClients'
 
 export default class S3 extends React.Component {
   render() {
     document.title = `S3 | ${ Const.BASE_PAGE_TITLE }`
+    getS3Buckets()
     return (
       <div>
         <h3>S3 バケット一覧</h3>
@@ -12,4 +14,9 @@ export default class S3 extends React.Component {
       </div>
     )
   }
+}
+
+async function getS3Buckets() {
+  const s3Buckets = await AwsClients.s3.listBuckets().promise()
+  console.log(s3Buckets.Buckets!)
 }
