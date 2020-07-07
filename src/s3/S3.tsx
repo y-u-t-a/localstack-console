4fbeb276ed3afb05809d707151a56c0a70b75f4d
Yuta Ueno <30320735+y-u-t-a@users.noreply.github.com>
2020-07-08 04:55:49 +0900

    S3 バケット一覧は S3BucketList が持つべき state

--- src/s3/S3.tsx
+++ src/s3/S3.tsx
@@ -1,41 +1,15 @@
 import React from 'react'
 import Const from '../common/Const'
 import S3BucketList from './S3BucketList'
-import AwsClients from '../common/AwsClients'
 
-interface Props {}
-
-interface State {
-  s3Buckets: Array<String>
-}
-
-export default class S3 extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props)
-    // state はコンストラクタで初期化する
-    this.state = {
-      s3Buckets: []
-    }
-  }
-  async componentDidMount() {
-    // 初回の検索結果は componentDidMount でセットする
-    this.setState({
-      s3Buckets: await getS3Buckets()
-    })
-  }
+export default class S3 extends React.Component {
   render() {
     document.title = `S3 | ${ Const.BASE_PAGE_TITLE }`
     return (
       <div>
         <h3>S3 バケット一覧</h3>
-        <S3BucketList s3Buckets={this.state.s3Buckets} />
+        <S3BucketList />
       </div>
     )
   }
-}
-
-async function getS3Buckets(): Promise<Array<String>> {
-  const s3Buckets = await AwsClients.s3.listBuckets().promise()
-  const s3BucketsName = s3Buckets.Buckets!.map(s3Bucket => s3Bucket.Name!)
-  return s3BucketsName
 }
--- src/s3/S3BucketList.tsx
+++ src/s3/S3BucketList.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
+import AwsClients from '../common/AwsClients'
 
-interface Props {
+interface Props {}
+
+interface State {
   s3Buckets: Array<String>
 }
 
-export default class S3BucketList extends React.Component<Props> {
+export default class S3BucketList extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+    // state は constructor で初期化する
+    this.state = {
+      s3Buckets: []
+    }
+  }
+  async componentDidMount() {
+    // 初回の検索結果は componentDidMount でセットする
+    this.setState({
+      s3Buckets: await getS3Buckets()
+    })
+  }
   render() {
-    const listElm = this.props.s3Buckets.map(s3Bucket => {
+    const listElm = this.state.s3Buckets.map(s3Bucket => {
       return (
         <li key={s3Bucket.toString()}>
           <a href={document.URL + "/" + s3Bucket.toString()}>{s3Bucket}</a>
@@ -19,4 +35,13 @@ export default class S3BucketList extends React.Component<Props> {
       </ul>
     )
   }
+}
+
+/**
+ * S3 バケットリストを参照し、S3 バケットの名前の配列を返却
+ */
+async function getS3Buckets(): Promise<Array<String>> {
+  const s3Buckets = await AwsClients.s3.listBuckets().promise()
+  const s3BucketsName = s3Buckets.Buckets!.map(s3Bucket => s3Bucket.Name!)
+  return s3BucketsName
 }
