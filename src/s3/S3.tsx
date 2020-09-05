c0501495a919a3070b920c2494600ac4dda9c875
Yuta Ueno <30320735+y-u-t-a@users.noreply.github.com>
2020-09-05 17:14:30 +0900

    Container で括った方が見栄えがいい

--- src/s3/S3.tsx
+++ src/s3/S3.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import Const from '../common/Const'
 import S3BucketList from './S3BucketList'
+import { Container } from 'react-bootstrap'
 
 export default class S3 extends React.Component {
   render() {
     document.title = `S3 | ${ Const.BASE_PAGE_TITLE }`
     return (
-      <div>
+      <Container>
         <h3>S3 バケット一覧</h3>
         <S3BucketList />
-      </div>
+      </Container>
     )
   }
 }
