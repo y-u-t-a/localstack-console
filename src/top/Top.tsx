2144aa5438f3f8ce8f6c51300660cc3dea459271
Yuta Ueno <30320735+y-u-t-a@users.noreply.github.com>
2020-09-06 20:03:58 +0900

    Top のデザインを S3 に合わせる

--- src/top/Top.tsx
+++ src/top/Top.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import Const from '../common/Const'
+import { Container } from 'react-bootstrap'
 
 export default class Top extends React.Component {
   render() {
     document.title = Const.BASE_PAGE_TITLE
     return (
-      <div>
-        <h3>サービス一覧</h3>
+      <Container>
+        <h1>サービス一覧</h1>
         <ul>
           <li><a href="/s3">S3</a></li>
           <li><a href="/vpc">VPC</a></li>
           <li><a href="/ec2">EC2</a></li>
         </ul>
-      </div>
+      </Container>
     )
   }
 }
