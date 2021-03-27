1e4f370fa8bb52435ece3e58b93f7b5ccb7ba45d
Yuta Ueno <30320735+y-u-t-a@users.noreply.github.com>
2021-03-28 06:05:55 +0900

    バケットのトップページにオブジェクト一覧を表示

--- next-version/interfaces/index.ts
+++ next-version/interfaces/index.ts
@@ -1,4 +1,10 @@
 export type S3Bucket = {
   Name : string,
-  CreationDate : string
+  CreationDate : string,
+}
+
+export type S3Object = {
+  Key: string,
+  Size: number,
+  LastModified: string,
 }
--- next-version/pages/s3/[bucket]/index.tsx
+++ next-version/pages/s3/[bucket]/index.tsx
@@ -1,15 +1,42 @@
-import { useRouter } from 'next/router'
+import { GetServerSideProps } from 'next'
 
+import { S3 } from '../../../utils/aws-sdk-client'
+import { S3Object } from '../../../interfaces'
 import Layout from '../../../components/Layout'
 
-const S3BucketPage = () => {
-  const { bucket } = useRouter().query
+type Props = {
+  bucket: string,
+  s3Objects: S3Object[],
+}
 
+const S3BucketPage = (props: Props) => {
   return (
-    <Layout title={`S3 - ${ bucket } | AWS Mock`}>
-      <h1>バケット: { bucket }</h1>
+    <Layout title={`S3 - ${ props.bucket } | AWS Mock`}>
+      <h1>バケット: { props.bucket }</h1>
+      <ul>
+      { props.s3Objects.map( s3Object => (
+        <li key={s3Object.Key}>
+          <p>{ `${s3Object.Key} ${s3Object.Size} ${s3Object.LastModified}` }</p>
+        </li>
+      ))}
+      </ul>
     </Layout>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const bucket = context.query.bucket
+  const response = await S3.listObjectsV2({ Bucket: bucket.toString() }).promise()
+  const s3Objects:S3Object[] = response.Contents!.map( content => {
+    return {
+        Key: content.Key!,
+        Size: content.Size!,
+        LastModified: content.LastModified!.toLocaleString()
+    }
+  })
+  return {
+    props: { s3Objects, bucket }
+  }
 }
 
 export default S3BucketPage
