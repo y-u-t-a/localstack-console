dfd36a8383a0e53cf75a46a41d617e4ca94b623d
Yuta Ueno <30320735+y-u-t-a@users.noreply.github.com>
2021-04-04 14:33:11 +0900

    Update index.tsx

--- next-version/pages/index.tsx
+++ next-version/pages/index.tsx
@@ -1,8 +1,15 @@
+import Link from 'next/link'
+
 import Layout from '../components/Layout'
 
 const IndexPage = () => (
   <Layout title="AWS Mock">
     <h1>AWS Mock 😺</h1>
+    <ul>
+      <li>
+        <Link href='/s3'><a>S3</a></Link>
+      </li>
+    </ul>
   </Layout>
 )
 
