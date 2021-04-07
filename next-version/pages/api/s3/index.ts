c3040ae2f21cd943e647c851cba2fe4e5d33e38b
Yuta Ueno <30320735+y-u-t-a@users.noreply.github.com>
2021-04-07 22:19:51 +0900

    バケットリスト取得APIを追加

---
+++ next-version/pages/api/s3/index.ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import { getBucketList } from '../../../utils/s3'
+
+export default async (req:NextApiRequest, res:NextApiResponse) => {
+  switch (req.method) {
+    case 'GET':
+      try {
+        const s3Buckets = await getBucketList()
+        res.status(200).json(s3Buckets)
+      } catch (error) {
+        res.status(500).json({message: error})
+      }
+      break
+    default:
+      res.status(404).end()
+      break
+  }
+}
