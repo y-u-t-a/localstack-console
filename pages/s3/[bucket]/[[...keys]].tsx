f43318421b4d1526c0867f10bcb0e22bd663849d
Yuta Ueno <30320735+y-u-t-a@users.noreply.github.com>
2021-05-08 02:37:19 +0900

    S3オブジェクトページを CSR に変更

--- pages/s3/[bucket]/[[...keys]].tsx
+++ pages/s3/[bucket]/[[...keys]].tsx
@@ -1,37 +1,38 @@
-import { GetServerSideProps } from 'next'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 
-import { getObjectList } from '../../../utils/s3'
 import { S3Object } from '../../../interfaces/s3'
 import Layout from '../../../components/Layout'
 import S3ObjectDetail from '../../../components/s3/S3ObjectDetail'
 import S3ObjectList from '../../../components/s3/S3ObjectList'
 
-type Props = {
-  bucket: string,
-  s3Objects: S3Object[],
-}
-
-const S3ObjectPage = (props: Props) => {
+const S3ObjectPage = () => {
+  const router = useRouter()
+  const bucket = router.query['bucket'] as string
+  const [s3Objects, setS3Objects] = useState<S3Object[]>([])
+  const fetchS3Objects = async () => {
+    if (router.isReady) {
+      const bucket = router.query['bucket'] as string
+      const keys = router.query['keys'] as string[] || []
+      const response = await fetch(`/api/s3/list/${bucket}/${keys.join('/')}`)
+      const body = await response.json()
+      setS3Objects(body)
+    }
+  }
+  useEffect(() => {
+    fetchS3Objects()
+  }, [router.query]) // URL が変更されるたびに実行する
   return (
-    <Layout title={`S3 - ${ props.bucket } | AWS Mock`}>
-      <h1>バケット: { props.bucket }</h1>
-      {props.s3Objects.length > 0 &&
-        <S3ObjectList bucket={props.bucket} s3Objects={props.s3Objects} />
+    <Layout title={`S3 - ${ bucket } | AWS Mock`}>
+      <h1>バケット: { bucket }</h1>
+      {s3Objects.length > 0 &&
+        <S3ObjectList bucket={bucket} s3Objects={s3Objects} />
       }
-      {props.s3Objects.length == 0 &&
-        <S3ObjectDetail bucket={props.bucket} />
+      {s3Objects.length == 0 &&
+        <S3ObjectDetail bucket={bucket} />
       }
     </Layout>
   )
-}
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const bucket = context.query.bucket as string
-  const keys = context.query.keys as string[] || []
-  const s3Objects = await getObjectList(bucket, keys.join('/'))
-  return {
-    props: { s3Objects, bucket }
-  }
 }
 
 export default S3ObjectPage
