e3dded5b346c2fff55b9b35b61854138de2ca8cc
Yuta Ueno <30320735+y-u-t-a@users.noreply.github.com>
2021-05-09 01:10:35 +0900

    オブジェクトの詳細表示かリストを表示するかの判定を正しくする

--- components/s3/S3ObjectDetail.tsx
+++ components/s3/S3ObjectDetail.tsx
@@ -1,26 +1,15 @@
-import { useEffect, useState } from 'react'
 import { S3Object } from '../../interfaces/s3'
 
 type Props = {
-  bucket: string,
-  objectKey: string,
+  s3Object: S3Object,
 }
 
 const S3ObjectDetail = (props:Props) => {
-  const [s3Object, setS3Object] = useState<S3Object>()
-  const fetchS3ObjectDetail = async () => {
-    const response = await fetch(`/api/s3/detail/${props.bucket}/${props.objectKey}`)
-    const body:S3Object = await response.json()
-    setS3Object(body)
-  }
-  useEffect(() => {
-    fetchS3ObjectDetail()
-  }, [])
   return (
     <>
       <p>オブジェクト詳細</p>
-      <p>ファイル名 : {s3Object?.Key}</p>
-      <p>更新日時 : {s3Object?.LastModified}</p>
+      <p>ファイル名 : {props.s3Object.Key}</p>
+      <p>更新日時 : {props.s3Object.LastModified}</p>
     </>
   )
 }
--- pages/s3/[bucket]/[[...keys]].tsx
+++ pages/s3/[bucket]/[[...keys]].tsx
@@ -9,28 +9,38 @@ import S3ObjectList from '../../../components/s3/S3ObjectList'
 const S3ObjectPage = () => {
   const router = useRouter()
   const bucket = router.query.bucket as string
-  const keys = router.query.keys as string[] || []
   const [s3Objects, setS3Objects] = useState<S3Object[]>([])
+  const [s3Object, setS3Object] = useState<S3Object>()
+  const [loading, setLoading] = useState<boolean>(true)
   const fetchS3Objects = async () => {
     if (router.isReady) {
       const bucket = router.query.bucket as string
       const keys = router.query.keys as string[] || []
-      const response = await fetch(`/api/s3/list/${bucket}/${keys.join('/')}`)
-      const body = await response.json()
-      setS3Objects(body)
+      let response = await fetch(`/api/s3/detail/${bucket}/${keys.join('/')}`)
+      if (response.ok) {
+        setS3Object(await response.json())
+      } else {
+        response = await fetch(`/api/s3/list/${bucket}/${keys.join('/')}`)
+        setS3Objects(await response.json())
+      }
+      setLoading(false)
     }
   }
   useEffect(() => {
+    setLoading(true)
+    setS3Object(undefined)
+    setS3Objects([])
     fetchS3Objects()
   }, [router.query]) // URL が変更されるたびに実行する
+
   return (
     <Layout title={`S3 - ${ bucket } | AWS Mock`}>
       <h1>バケット: { bucket }</h1>
-      {s3Objects.length != 1 &&
+      {!loading && !s3Object && // 苦し紛れだがオブジェクト詳細が取得できたか否かでリストを表示するか判断
         <S3ObjectList bucket={bucket} s3Objects={s3Objects} />
       }
-      {s3Objects.length == 1 &&
-        <S3ObjectDetail bucket={bucket} objectKey={keys.join('/')} />
+      {!loading && s3Object &&
+        <S3ObjectDetail s3Object={s3Object} />
       }
     </Layout>
   )
