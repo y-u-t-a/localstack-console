712655623a159cbb9ec9830aa19d437e41805fb5
Yuta Ueno <30320735+y-u-t-a@users.noreply.github.com>
2021-05-12 02:36:04 +0900

    CreateBucket を v3 に移行

--- pages/api/s3/new/bucket.ts
+++ pages/api/s3/new/bucket.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-import { S3v2 } from '../../../../utils/aws-sdk-client'
+import { S3v3Client } from '../../../../utils/aws-sdk-client'
+import { CreateBucketCommand } from '@aws-sdk/client-s3'
 import { S3Bucket } from '../../../../interfaces/s3'
 
 export default async (req:NextApiRequest, res:NextApiResponse) => {
@@ -8,10 +9,13 @@ export default async (req:NextApiRequest, res:NextApiResponse) => {
     case 'POST':
       try {
         const body:S3Bucket = JSON.parse(req.body)
-        await S3v2.createBucket({Bucket: body.Name}).promise()
+        const command = new CreateBucketCommand({
+          Bucket: body.Name
+        })
+        await S3v3Client.send(command)
         res.status(200).end()
       } catch (error) {
-        res.status(500).json({message: error.code})
+        res.status(500).json({message: error.toString()})
       }
       break
     default:
