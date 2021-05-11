1a52ce9c0cbe66d2a202973c5274dada055c6d95
Yuta Ueno <30320735+y-u-t-a@users.noreply.github.com>
2021-05-12 03:18:31 +0900

    S3v2 の使用箇所がなくなったので削除

--- utils/aws-sdk-client.ts
+++ utils/aws-sdk-client.ts
@@ -1,16 +1,4 @@
-import AWS from 'aws-sdk'
 import { S3Client, S3ClientConfig } from '@aws-sdk/client-s3'
-
-const mockConfig = {
-  endpoint : "http://localhost:4566",
-  region : "us-east-1",
-  credential : new AWS.Credentials({
-    accessKeyId: 'dummy',
-    secretAccessKey: 'dummy',
-    sessionToken: 'dummy',
-  }),
-  s3ForcePathStyle: true,
-}
 
 const mockConfigV3:S3ClientConfig = {
   endpoint : "http://localhost:4566",
@@ -23,5 +11,4 @@ const mockConfigV3:S3ClientConfig = {
   forcePathStyle: true,
 }
 
-export const S3v2 = new AWS.S3(process.env.TARGET === 'mock' ? mockConfig : {})
 export const S3v3Client = new S3Client(process.env.TARGET === 'mock' ? mockConfigV3 : {})
