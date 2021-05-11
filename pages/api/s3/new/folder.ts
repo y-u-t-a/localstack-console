ffded5fe82cc01e4fef7373c3f649de059f93070
Yuta Ueno <30320735+y-u-t-a@users.noreply.github.com>
2021-05-12 04:24:29 +0900

    フォルダにスラッシュが含まれるときにエラーにする

--- pages/api/s3/new/folder.ts
+++ pages/api/s3/new/folder.ts
@@ -5,10 +5,13 @@ import { PutObjectCommand } from '@aws-sdk/client-s3'
 import { CreateS3FolderApiRequest } from '../../../../interfaces/s3'
 
 export default async (req:NextApiRequest, res:NextApiResponse) => {
-  const body:CreateS3FolderApiRequest = JSON.parse(req.body)
   switch (req.method) {
     case 'POST':
       try {
+        const body:CreateS3FolderApiRequest = JSON.parse(req.body)
+        if (body.folderName.indexOf('/') != -1) {
+          throw Error("フォルダーにスラッシュを含めることはできません")
+        }
         // 末尾をスラッシュにすることでフォルダになる
         const key = body.prefix.length === 0?
           `${body.folderName}/` : `${body.prefix}/${body.folderName}/`
