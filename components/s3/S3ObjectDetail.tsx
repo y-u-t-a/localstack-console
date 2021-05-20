e703b8f3f2cf2d969065670ce511d16c65359013
Yuta Ueno <30320735+y-u-t-a@users.noreply.github.com>
2021-05-21 00:01:12 +0900

    オブジェクト詳細画面にダウンロードボタンを追加

--- components/s3/S3ObjectDetail.tsx
+++ components/s3/S3ObjectDetail.tsx
@@ -1,3 +1,5 @@
+import { Button } from '@material-ui/core'
+
 import { S3Object } from '../../interfaces/s3'
 
 type Props = {
@@ -7,6 +9,10 @@ type Props = {
 const S3ObjectDetail = (props:Props) => {
   return (
     <>
+      <Button
+        variant="contained"
+        href={`/api/s3/download/${props.s3Object.Bucket}/${props.s3Object.Key}`}
+      >ダウンロード</Button>
       <p>オブジェクト詳細</p>
       <p>ファイル名 : {props.s3Object.Key}</p>
       <p>更新日時 : {props.s3Object.LastModified}</p>
